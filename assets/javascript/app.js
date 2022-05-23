51fb4537649d3765cc09b4a2815beef21a57c8fc
cmac <chelsey.macneill@gmail.com>
2022-05-22 19:03:55 -0700

    Flushed out how I want the quiz to flow and added the skeleton of comments to the app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,29 @@
 var card = $("#quiz-area");
 var countStartNumber = 30; 
 
-// Question set 
+
+// I want some questions that are asking for a definition and then the user types in thier answer. 
+// They then are shown the correct definition 
+// Asked if they got it right (y/n) 
+// It then moves to the next question 
+
+// Other questions will just be T/F or pick A,B,C,D
+// These questions the game can check the answer
+
+// At the end show the score 
+
+
+// Question set: Definition question sets
 var questions = [{
     question: "Profiling as defined by GDPR means:",
     answers: [ "A: the automated analysis or predicting of behavior, location, movements, reliability, interests, personal preferences, health, economic situation, performance, etc."]
 }];
+
+// Question set: T/F Question sets
+
+
+// Question set: A,B,C,D Questions
+
 
 
 // Variable to hold our setInterval 
